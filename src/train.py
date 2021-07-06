dd116e9a0ec4fd09a925842bd70e90585a9d1d32
David Stephens <5738970+davesteps@users.noreply.github.com>
2021-07-06 08:59:27 +0000

    baseline model

--- src/train.py
+++ src/train.py
@@ -1,13 +1,20 @@
 import pandas as pd
+import yaml
 from sklearn.ensemble import RandomForestClassifier
 import joblib
+
+with open("./params.yaml", 'r') as s:
+    params = yaml.safe_load(s)['train']
 
 train = pd.read_csv('data/train.csv')
 
 X = train.drop('shiptype3',axis=1)
 y = train['shiptype3']
 
-clf = RandomForestClassifier(max_depth=2, random_state=0)
+clf = RandomForestClassifier(max_depth=params['max_depth'],
+                             n_estimators = params['n_estimators'],
+                             min_samples_split = params['min_samples_split'],
+                             random_state=0)
 
 clf.fit(X, y)
 
